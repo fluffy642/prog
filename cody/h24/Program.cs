558892884e47bbe8dc9d1f3314a13972efde0a9d
Kyrylo <st026333@student.cichnovabrno.cz>
2026-04-23 09:37:59 +0200

    h24

---
+++ cody/h24/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Student student2 = new Student("Roman", 16, "IT3A");
+        student2.AddMark(5);
+        student2.AddMark(1);
+        student2.ShowInfo();
+        student2.RemoveMark(5);
+        student2.ShowInfo();
+        student2.AddMark(5);
+        student2.AddMark(3);
+        student2.AddMark(1);
+        Console.WriteLine(student2.AverageMarks());
+        student2.ShowInfo();
+        Console.WriteLine(student2.IfAdult());
+    }
+}
---
+++ cody/h24/Student.cs
@@ -0,0 +1,53 @@
+﻿public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Class { get; set; }
+    public List<int> Marks;
+
+    public Student(string jmeno, int vek, string trida)
+    {
+        Name = jmeno;
+        Age = vek;
+        Class = trida;
+        Marks = new List<int>();
+    }
+
+    public void AddMark(int znamka)
+    {
+        Marks.Add(znamka);
+    }
+    public void RemoveMark(int znamka)
+    {
+        Marks.Remove(znamka);
+    }
+    public void ShowInfo()
+    {
+        Console.WriteLine("Jmeno studenta: " + Name);
+        Console.WriteLine("Vek studenta: " + Age);
+        Console.WriteLine("Trida studenta: " + Class);
+        Console.Write("Znamky studenta: ");
+        foreach (int znamka in Marks)
+        {
+            Console.Write(znamka + " ");
+        }
+        Console.WriteLine("\n");
+    }
+    public void ChangeClass(string trida)
+    {
+        Class = trida;
+    }
+    public double AverageMarks()
+    {
+        double res = 0;
+        foreach (int znamka in Marks)
+        {
+            res += znamka;
+        }
+        return res / Marks.Count;
+    }
+    public bool IfAdult()
+    {
+        return Age >= 18;
+    }
+}
