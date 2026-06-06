3d7a71fccf8b698fe239bee95bfbc794d9a27fa4
Kyrylo Kolotiievskyi <st026333@student.cichnovabrno.cz>
2026-06-06 13:46:21 +0200

    Update Program.cs

--- cody/cichnamoni/Program.cs
+++ cody/cichnamoni/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.Design;
+using System;
+using System.Collections.Generic;
 
 public class Program
 {
@@ -32,88 +33,102 @@ public class Program
 
         Console.WriteLine($"\nZacatek hry");
 
-        bool pokracovat = true;
+        bool pokracovat = true, spravnyVyber = false;
         bool dostupneUzraveni = true, dostupnySpecUtok = true;
         bool dostupneUzraveniTrenera = true, dostupnySpecUtokTrenera = true;
-        int vyber, vyberTrenera;
+        int vyber = 99, vyberTrenera = 99;
 
         for (int i = 1; pokracovat == true; i++)
         {
+            Console.WriteLine($"\n{i}. tah:");
             Console.WriteLine($"\nTvuj cichnamon: ");
             cichnamon.VypisInfo();
-            Console.WriteLine($"\nCichnamon trenere: ");
+            Console.WriteLine($"\nCichnamon trenera: ");
             cichnatrener.VypisInfo();
 
             Console.WriteLine("\nTvuj tah, moznosti: ");
-            Console.WriteLine(" 1 = Zakladni utok (pocita i cichnamonuv bonus)");
-            Console.WriteLine(" 2 = Specialni utok (jen co 2 tah, a nelze pouzivat 2 krat po sobe)");
-            Console.WriteLine(" 3 = Uzdraveni (jen jednou za utok)");
-            Console.Write("Vyber co chces udelat: ");
-            vyber = int.Parse(Console.ReadLine());
-            if (vyber == 1)
-            {
-                cichnamon.ProvestUtokZakl(cichnamon, cichnatrener);
-                Console.Write($"Byl provedeny zakladni utok {cichnamon.UtokZakl.Nazev}");
-                dostupnySpecUtok = true;
-            }
-            else if (vyber == 2 && dostupnySpecUtok == true && i >= 2)
-            {
-                cichnamon.ProvestUtokSpec(cichnamon, cichnatrener);
-                Console.Write($"Byl provedeny zakladni utok {cichnamon.UtokSpec.Nazev}");
-                dostupnySpecUtok = false;
-            }
-            else if (vyber == 3 && dostupneUzraveni == true)
-            {
-                int uzdraveni = new Random().Next(20, 40);
-                cichnamon.Uzdraveni(uzdraveni);
-                Console.Write($"Bylo provedeno uzdraveni o {uzdraveni} HP");
-                dostupnySpecUtok = true;
-                dostupneUzraveni = false;
-            }
-            else
-            {
-                Console.WriteLine("Spatny vyber");
-            }
-
-            Console.WriteLine("\nTah trenera: ");
-            vyberTrenera = new Random().Next(1, 4);
-
-            if (vyberTrenera == 1)
-            {
-                cichnatrener.ProvestUtokZakl(cichnatrener, cichnamon);
-                Console.Write($"Byl provedeny zakladni utok {cichnatrener.UtokZakl.Nazev}");
-                dostupnySpecUtokTrenera = true;
-            }
-            else if (vyberTrenera == 2 && dostupnySpecUtokTrenera == true && i >= 2)
-            {
-                cichnatrener.ProvestUtokSpec(cichnatrener, cichnamon);
-                Console.Write($"Byl provedeny zakladni utok {cichnatrener.UtokSpec.Nazev}");
-                dostupnySpecUtokTrenera = false;
-            }
-            else if (vyberTrenera == 3 && dostupneUzraveniTrenera == true)
-            {
-                int uzdraveni = new Random().Next(20, 40);
-                cichnatrener.Uzdraveni(uzdraveni);
-                Console.Write($"Bylo provedeno uzdraveni o {uzdraveni} HP");
-                dostupnySpecUtokTrenera = true;
-                dostupneUzraveniTrenera = false;
-            }
-            else
-            {
-                Console.Write("Spatny vyber trenera");
+            
+            while(!spravnyVyber) {
+                Console.WriteLine("\n1 = Zakladni utok (pocita i cichnamonuv bonus):");
+                cichnamon.UtokZakl.VypisInfo();
+                Console.WriteLine("\n2 = Specialni utok (jen co 2 tah, a nelze pouzivat 2 krat po sobe):");
+                cichnamon.UtokSpec.VypisInfo();
+                Console.WriteLine("\n3 = Uzdraveni o nahodne HP od 20 do 40 (jen jednou za hru)");
+                Console.Write("\nVyber co chces udelat: ");
+                vyber = int.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                if (vyber == 1)
+                {
+                    cichnamon.ProvestUtokZakl(cichnamon, cichnatrener);
+                    Console.WriteLine($"  Byl provedeny zakladni utok \"{cichnamon.UtokZakl.Nazev}\"");
+                    dostupnySpecUtok = true;
+                    spravnyVyber = true;
+                }
+                else if (vyber == 2 && dostupnySpecUtok == true && i >= 2)
+                {
+                    cichnamon.ProvestUtokSpec(cichnamon, cichnatrener);
+                    Console.WriteLine($"  Byl provedeny zakladni utok \"{cichnamon.UtokSpec.Nazev}\"");
+                    dostupnySpecUtok = false;
+                    spravnyVyber = true;
+                }
+                else if (vyber == 3 && dostupneUzraveni == true)
+                {
+                    int uzdraveni = new Random().Next(20, 40);
+                    cichnamon.Uzdraveni(uzdraveni);
+                    Console.WriteLine($"  Bylo provedeno uzdraveni o {uzdraveni} HP");
+                    dostupnySpecUtok = true;
+                    dostupneUzraveni = false;
+                    spravnyVyber = true;
+                }
+                else
+                {
+                    Console.WriteLine("  Spatny vyber, skus znovu");
+                }
             }
 
-            Console.Write($"Konec {i}. tahu");
+            Console.WriteLine("\nTah trenera: \n");
+            spravnyVyber = false;
+            while(!spravnyVyber) {
+                vyberTrenera = new Random().Next(1, 4);
+            
+                if (vyberTrenera == 1)
+                {
+                    cichnatrener.ProvestUtokZakl(cichnatrener, cichnamon);
+                    Console.WriteLine($"  Byl provedeny zakladni utok \"{cichnatrener.UtokZakl.Nazev}\"");
+                    dostupnySpecUtokTrenera = true;
+                    spravnyVyber = true;
+                }
+                else if (vyberTrenera == 2 && dostupnySpecUtokTrenera == true && i >= 2)
+                {
+                    cichnatrener.ProvestUtokSpec(cichnatrener, cichnamon);
+                    Console.WriteLine($"  Byl provedeny zakladni utok \"{cichnatrener.UtokSpec.Nazev}\"");
+                    dostupnySpecUtokTrenera = false;
+                    spravnyVyber = true;
+                }
+                else if (vyberTrenera == 3 && dostupneUzraveniTrenera == true)
+                {
+                    int uzdraveni = new Random().Next(20, 40);
+                    cichnatrener.Uzdraveni(uzdraveni);
+                    Console.WriteLine($"  Bylo provedeno uzdraveni o {uzdraveni} HP");
+                    dostupnySpecUtokTrenera = true;
+                    dostupneUzraveniTrenera = false;
+                    spravnyVyber = true;
+                }
+            }
 
-            if (cichnamon.HP <= 0)
+            Console.WriteLine($"\nKonec {i}. tahu\n");
+            spravnyVyber = false;
+
+            if (!cichnamon.JeNazivu())
             {
                 pokracovat = false;
-                Console.Write($"Hra ukoncena. \nZvitezil {trener.Jmeno}");
+                Console.WriteLine($"Hra ukoncena. \n\nZvitezil {trener.Jmeno}");
+                trener.Level++;
             } 
-            else if(cichnatrener.HP <= 0)
+            else if(!cichnatrener.JeNazivu())
             {
                 pokracovat = false;
-                Console.Write($"Hra ukoncena. \nZvitezil {jmeno}");
+                Console.WriteLine($"Hra ukoncena. \n\nZvitezil {jmeno}");
             }
         }
     }
