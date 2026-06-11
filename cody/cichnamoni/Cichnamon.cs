a5d5dbc11afc76e720e07fc1741738b7ef755792
Kyrylo Kolotiievskyi <st026333@student.cichnovabrno.cz>
2026-06-11 16:13:18 +0200

    Update Cichnamon.cs

--- cody/cichnamoni/Cichnamon.cs
+++ cody/cichnamoni/Cichnamon.cs
@@ -2,49 +2,59 @@ using System;
 
 public class Cichnamon
 {
-	public string Jmeno { get; set; }
-	public int MaxHP { get; set; }
-	public int HP;
-	public int Bonus { get; set; }
-	public Utok UtokZakl { get; set; }
-	public Utok UtokSpec { get; set; }
+    public string Jmeno { get; set; }
+    public int MaxHP { get; set; }
+    public int HP;
+    public int Bonus { get; set; }
+    public Utok UtokZakl { get; set; }
+    public Utok UtokSpec { get; set; }
 
-	public Cichnamon(string jmeno, int maxHP, int bonus, string nazevUtokuZakl, int poskozeniZakl, string popisZakl, string nazevUtokuSpec, int poskozeniSpec, string popisSpec)
-	{
-		Jmeno = jmeno;
-		MaxHP = maxHP;
-		HP = maxHP;
-		Bonus = bonus;
-		UtokZakl = new Utok(nazevUtokuZakl, poskozeniZakl, popisZakl);
-		UtokSpec = new Utok(nazevUtokuSpec, poskozeniSpec, popisSpec);
-	}
+    public Cichnamon(string jmeno, int maxHP, int bonus, string nazevUtokuZakl, int poskozeniZakl, string popisZakl, string nazevUtokuSpec, int poskozeniSpec, string popisSpec)
+    {
+        Jmeno = jmeno;
+        MaxHP = maxHP;
+        HP = maxHP;
+        Bonus = bonus;
+        UtokZakl = new Utok(nazevUtokuZakl, poskozeniZakl, popisZakl);
+        UtokSpec = new Utok(nazevUtokuSpec, poskozeniSpec, popisSpec);
+    }
 
-	public void VypisInfo()
-	{
-		Console.WriteLine($"  Cichnamon \"{Jmeno}\"");
-		Console.WriteLine($"  Maximalni zdravi: {MaxHP} HP");
-		Console.WriteLine($"  Aktualni zdravi: {HP} HP");
-		Console.WriteLine($"  Ma utoky: \"{UtokZakl.Nazev}\" a \"{UtokSpec.Nazev}\"");
-		Console.WriteLine($"  Bonus k utoku: +{Bonus}");
-	}
+    public Cichnamon(string jmeno, int maxHP, int bonus, Utok utok1, Utok utok2)
+    {
+        Jmeno = jmeno;
+        MaxHP = maxHP;
+        HP = maxHP;
+        Bonus = bonus;
+        UtokZakl = utok1;
+        UtokSpec = utok2;
+    }
 
-	public void ProvestUtokZakl(Cichnamon hrac, Cichnamon protihrac)
-	{
-		protihrac.HP -= hrac.UtokZakl.Poskozeni;
-	}
+    public void VypisInfo()
+    {
+        Console.WriteLine($"  Cichnamon \"{Jmeno}\"");
+        Console.WriteLine($"  Maximalni zdravi: {MaxHP} HP");
+        Console.WriteLine($"  Aktualni zdravi: {HP} HP");
+        Console.WriteLine($"  Ma utoky: \"{UtokZakl.Nazev}\" a \"{UtokSpec.Nazev}\"");
+        Console.WriteLine($"  Bonus k utoku: +{Bonus}");
+    }
 
-	public void ProvestUtokSpec(Cichnamon hrac, Cichnamon protihrac)
-	{
-		protihrac.HP -= hrac.UtokSpec.Poskozeni;
-	}
+    public void ProvestUtokZakl(Cichnamon hrac, Cichnamon protihrac)
+    {
+        protihrac.HP -= hrac.UtokZakl.Poskozeni;
+    }
 
-	public void Uzdraveni(int hp)
-	{
-		HP += hp;
-	}
+    public void ProvestUtokSpec(Cichnamon hrac, Cichnamon protihrac)
+    {
+        protihrac.HP -= hrac.UtokSpec.Poskozeni;
+    }
 
-	public bool JeNazivu()
-	{
-		return HP >= 0;
-	}
+    public void Uzdraveni(int hp)
+    {
+        HP += hp;
+    }
+
+    public bool JeNazivu()
+    {
+        return HP >= 0;
+    }
 }
